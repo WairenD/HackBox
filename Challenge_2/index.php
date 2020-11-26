120bdd24e6845c295d14860668d77c5db2057775
Georgi768 <59054341+Georgi768@users.noreply.github.com>
2020-11-26 18:07:55 +0100

    added viewport

--- Challenge_2/index.php
+++ Challenge_2/index.php
@@ -3,6 +3,7 @@
     <head>
         <title>Challenge 2</title>
         <link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
     </head>
     <body>
