0eda5a5a50ec7905b434702d232cc2a65ca28cb2
jonathanmohamed <jonathanmohamed34@gmail.com>
2020-12-15 03:07:47 +0100

    Revert "Changed "Dropdown menu" to "Challenges""
    This reverts commit 428487f3b112e8aa41527202323817abff0ab954.

--- Challenge_1/index.php
+++ Challenge_1/index.php
@@ -22,7 +22,7 @@
                   <li><a href="../">Home</a></li>
                   <li><a href="../about.php">About</a></li>
                   <li>
-                      <a href="#" class="desktop-item">Challenges</a>
+                      <a href="#" class="desktop-item">Dropdown Menu</a>
                       <input type="checkbox" id="showDrop" />
                       <label for="showDrop" class="mobile-item">Challenges</label>
                       <ul class="drop-menu">
