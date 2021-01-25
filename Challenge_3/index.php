1aa28c9c9763ada52a9d2b567a44659bdb144e76
ThomasK24a <thomas_koops@hotmail.com>
2021-01-25 15:23:46 +0100

    Adding assistant to challenge 3

--- Challenge_3/index.php
+++ Challenge_3/index.php
@@ -93,6 +93,24 @@
                 </div>
             </nav>
         </header>
+        <footer>
+        <div class="main-content">
+            <div class="center box">
+                <h2>Location</h2>
+                <div class="content">
+                    <div class="place">
+                        <span class="fas fa-map-marker-alt"></span>
+                        <span class="text">NHL Stenden</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom">
+            <span class="credit">Created By <a href="#">HACKBOX 2.0</a> | </span>
+            <span class="far fa-copyright"></span> 2020 All rights reserved.
+            <span><a href="./privacy_policy.php">Privacy Policy</a></span>
+        </div>
+    </footer>
     <?php
     $dummyUserId = "25213";
     $dummyAuthToken = "d9gdsn0v51gqwezgj";
@@ -216,26 +234,13 @@
                 echo('<p class="error">Invalid role set</p>'); 
 			}
             ?>
-            </div>
+        </div>
+        
     </div>
-     <footer>
-        <div class="main-content">
-            <div class="center box">
-                <h2>Location</h2>
-                <div class="content">
-                    <div class="place">
-                        <span class="fas fa-map-marker-alt"></span>
-                        <span class="text">NHL Stenden</span>
-                    </div>
-                </div>
-            </div>
+    <div id="hint"> </div>
+        <div id="assistant">
+            <img src="images/assist-sarcastic.png" alt="assistant" onclick="getHint()">
         </div>
-        <div class="bottom">
-            <span class="credit">Created By <a href="#">HACKBOX 2.0</a> | </span>
-            <span class="far fa-copyright"></span> 2020 All rights reserved.
-            <span><a href="./privacy_policy.php">Privacy Policy</a></span>
-        </div>
-    </footer>
 </body>
 
 </html>
---
+++ Challenge_3/js/assistant.js
@@ -0,0 +1,27 @@
+const a = [
+    "As a user on this site we cannot do anything, we need to escalate our privileges check around the webpage if you can somehow change your role to an Admin",
+    "It seems that to access anything on this site you need a valid authorization token and user id.",
+    "The website seems to be storing user info on the user's side, press f12 and check around the Storage if you can't use that to your advantage."
+];
+
+var hints = 0;
+
+
+function getHint() {
+    $('#hint').show().delay(10000).hide(0);
+    setTimeout(function() {
+        $('#hint').fadeOut('fast');
+    }, 8000);
+
+    document.getElementById('hint').innerHTML = (a[this.hints]);
+    this.hints = this.hints < a.length - 1? ++this.hints: 0;
+}
+
+function getHintWithInput(hintText) {
+    $('#hint').show().delay(10000).hide(0);
+    setTimeout(function () {
+        $('#hint').fadeOut('fast');
+    }, 8000);
+
+    document.getElementById('hint').innerHTML = (hintText);
+}
