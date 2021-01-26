521a6d5df32ef7f116bc5b55d5ab21fcc106ff76
Georgi768 <59054341+Georgi768@users.noreply.github.com>
2021-01-26 14:42:49 +0200

    Challenge 2 added image and registration

--- Challenge_2/index.php
+++ Challenge_2/index.php
@@ -103,7 +103,7 @@
                             $username = $_POST['username'];
                             $password = $_POST['password'];
                             if (!empty($username) && !empty($password)) {
-                                if ($username == "bruteforce" && $password == "1234") {
+                                if ($username == "bruteforce" && $password == "9OXEMTG") {
                                     header("Location: ../story2.php");
                                 } else {
                                     echo '<p class="errorText">Incorrect username or password!</p>';
@@ -135,7 +135,7 @@
                     <p class="text">find the missing password</p>
                 </div>
                 <div class="images">
-                    <img src="images/logo.png" alt="a key ?" id="img-box">
+                    <img src="images/keyImage.png" alt="a key ?" id="img-box">
                     <div id="myModal" class="modal">
                         <span class="close">&times;</span>
                         <img class="modal-content" id="img01">
