f8435adc6f81cda4d67b42f81da5e5da49e8309f
be-easyy <48102175+be-easyy@users.noreply.github.com>
2021-01-27 13:30:53 +0100

    Add files via upload
    javascript for end button

--- Challenge_5/index.php
+++ Challenge_5/index.php
@@ -10,14 +10,16 @@
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>Challenge 5</title>
 </head>
-<?php 
-    include "../header2.php";
-    include "../footer2.php";
+<?php
+include "../header2.php";
 ?>
 
 <body id="body" background="bg1.jpg">
 <h3> After completing previous challenges its time to get the location of the terrorist by getting their IP Address</h3>
 <h3> You have managed to start a conversation with him and need to inject some Cross Site Scripting (Javascript using an api) to acquire the IP address. (Shown Below)</h3>
+
+
+
 <div class="mainChallengeText" style="float: none">
     <div id="1" class="leftTextDiv"  style="display: none">
         <p style="margin-top:20px;" class="textRight"><img class="avatarleft" src="logo.png" alt="Avatar"> What do you think you’re doing? Who are you and why are you interfering in my affairs?</p>
@@ -32,18 +34,19 @@
         <span class="textRightSpace"></span>
     </div>
     <div id="4" class="rightTextDiv" style="display: none">
-        <p style="margin-top:20px;" class="textLeft"><img class="avatar-right" src="2.png" alt="Avatar" class="right">It doesn’t matter who I am. I know who you are, Minerva.You’re hurting innocent people to get revenge on one person</p>
+        <p style="margin-top:20px;" class="textLeft"><img class="avatar-right" src="2.png" alt="Avatar" class="right">Incase you didnt notice i just sent some malicious scripting and we now have your IP address and location, you wont get away with this</p>
         <span class="textLeftSpace"></span>
     </div>
     <div id="5" class="leftTextDiv" style="display: none">
-        <p style="margin-top: 5px" class="textRight"><img class="avatarleft" src="logo.png" alt="Avatar"> Listen, punk. Until that vile corporation is nothing but a bad memory, or someone stops me, I won’t stop. Shiny Tech does not care for those they deem insignificant.</p>
+        <p style="margin-top: 5px" class="textRight"><img class="avatarleft" src="logo.png" alt="Avatar"> So i see you tried to send some malicious scripting, youre gonna have to try harder than that, you are never going to find me!!!.</p>
         <span class="textRightSpace"></span>
     </div>
-    <div class="actionTextDiv">
+    <div id="button" class="actionTextDiv" style="display: none">
         <a href="../story5.php"><input class="actionText" style="margin-top:20px; background-color:#3a3b3d; border:none; cursor:pointer; color:white;" type="submit" name="end" value="End"></a>
     </div>
 </div>
 <br>
+
     <div  id="question" class="container question"  style="display: none" >
 
         <form  action="index.php" method="post">
@@ -71,7 +74,7 @@
             $xxs = $_POST['xxs'];
             if ($xxs == "correct")
                 echo " <script>
-    document.getElementById('body').onload= setTimeout(Four,6000); setTimeout(question,7001); 
+    document.getElementById('body').onload= setTimeout(Four,6000); setTimeout(button,6500); setTimeout(question,7001); 
     function Four() {
         var x = document.getElementById('4');
         if (x.style.display === 'none') {
@@ -89,12 +92,22 @@
             x.style.display = 'none';
         }
         }
+        
+        function button() {
+        var x = document.getElementById('button');
+        if (x.style.display === 'none') {
+            x.style.display = 'block';
+        } else {
+            x.style.display = 'none';
+        }
+        }
+        
      </script> ";
 
             else {
 
                 echo " <script>
-    document.getElementById('body').onload= setTimeout(Five,6000); setTimeout(question,7001);
+    document.getElementById('body').onload= setTimeout(Five,6000); setTimeout(button,6500); setTimeout(question,7001);
     function Five() {
         var x = document.getElementById('5');
         if (x.style.display === 'none') {
@@ -112,12 +125,25 @@
             x.style.display = 'none';
         }
         }
+        function button() {
+        var x = document.getElementById('button');
+        if (x.style.display === 'none') {
+            x.style.display = 'block';
+        } else {
+            x.style.display = 'none';
+        }
+        }
+        
      </script>";
             }
         }
         ?>
 
     </div>
+<!--</div>-->
+
+
+
 <script>
     document.getElementById("body").onload= setTimeout(One,1500); setTimeout(Two,3000); setTimeout(Three,4500);setTimeout(question,7000);
     function One() {
@@ -155,6 +181,33 @@
             x.style.display = "none";
         }
     }
+
 </script>
+
+<br>
+
+
+
 </body>
+
+<footer>
+    <div class="main-content">
+        <div class="center box">
+            <h2>
+                Location</h2>
+            <div class="content">
+                <div class="place">
+                    <span class="fas fa-map-marker-alt"></span>
+                    <span class="text">NHL Stenden</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <span class="credit">Created By <a href="#">HACKBOX 2.0</a> | </span>
+        <span class="far fa-copyright"></span> 2020 All rights reserved.
+        <span><a href="./privacy_policy.php">Privacy Policy</a></span>
+    </div>
+</footer>
+
 </html>
